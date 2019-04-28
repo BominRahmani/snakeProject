9568c65626fba8f112d9ef4476ea59006398b740
ky3979 <50038863+ky3979@users.noreply.github.com>
2019-04-28 16:53:24 -0400

    Update Snake.java

--- GroupProject1/src/Snake.java
+++ GroupProject1/src/Snake.java
@@ -1,25 +1,76 @@
+import javax.swing.*;
+import java.security.Key;
 import java.util.Random;
+import java.util.LinkedList;
+import java.awt.event.KeyEvent;
 
 public class Snake {
-    int size;
-    int score;
-    char [] [] board;
+
+    private LinkedList<Body> entity = new LinkedList<Body>();
+    private Body head;
+    private Body tail;
+    private int size;
 
     public Snake(){
-    board = new char[20][20];
-    size = 1;
-    score = 0;
+        int xPos = randomNum();
+        int yPos = randomNum();
+        head = new Body(xPos, yPos);
+        tail = head;
+        entity.add(head);
+        size = 1;
+        score = 0;
     }
 
-    //populate the board
-    public void BoardSetUp(int[][] board) {
-        for(int x = 0; x < board.length;x++){
-            for (int i = 0; i < board[0].length; i++) {
-                board[x][i] = '-';
-            }
-        }
-        board[(int)Math.random()* 20+1][(int)Math.random()*20 + 1] = 's';
+    public int randomNum() {
+        Random rand = new Random();
+        return rand.nextInt(20);
     }
-    
 
+    public LinkedList<Body> getEntity() {
+        return entity;
+    }
+
+    public void setEntity(LinkedList<Body> snake) {
+        entity = snake;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+
+    public Body getHead() {
+        return head;
+    }
+
+    public void setHead(Body head) {
+        this.head = head;
+    }
+
+    public Body getTail() {
+        return tail;
+    }
+
+    public void setTail(Body tail) {
+        this.tail = tail;
+    }
 }

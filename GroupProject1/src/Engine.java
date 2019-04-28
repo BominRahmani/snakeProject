183c6dff55b7b435d61143552cf37113961a86db
ky3979 <50038863+ky3979@users.noreply.github.com>
2019-04-28 16:58:34 -0400

    Update Engine.java

--- GroupProject1/src/Engine.java
+++ GroupProject1/src/Engine.java
@@ -1,6 +1,17 @@
-public class Engine {
-    public static void main(String[] args) {
-        Snake snake = new Snake();
+import javax.swing.*;
+import java.awt.*;
 
+public class engine extends JFrame {
+
+    static final char APPLE = 'A';
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.setSize(800, 600);
+        b.setVisible(true);
+
+        b.setSnake();
+        b.showBoard();
     }
+
 }

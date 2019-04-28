4676ff4241f3f1c3a9da22cdd54e4dc7142bc5a2
ky3979 <50038863+ky3979@users.noreply.github.com>
2019-04-28 19:44:24 -0400

    Add files via upload

---
+++ GroupProject1/src/Board.java
@@ -0,0 +1,57 @@
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Board extends JPanel implements Runnable {
+
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+    private final int pixel = 25;
+    private boolean gameOver;
+    private Body head;
+    private ArrayList<Body> SNAKE;
+    private int score;
+
+    public Board() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        head = new Body(randomNum(), randomNum(), pixel);
+        SNAKE = new ArrayList<Body>();
+        SNAKE.add(head);
+        score = 0;
+    }
+
+    public int randomNum() {
+        Random rand = new Random();
+        return rand.nextInt(20) * 25;
+    }
+
+    public int length() {
+        return SNAKE.size();
+    }
+
+    public void paintComponent(Graphics g) {
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        //Draw 20 x 20 grid
+        for (int i = 0; i < WIDTH / 25; i++) {
+            g.drawLine(i * 25, 0, i * 25, HEIGHT);
+        }
+        for (int i = 0; i < HEIGHT / 25; i++) {
+            g.drawLine(0, i * 25, HEIGHT, i * 25);
+        }
+
+        //Place the snake on the grid
+        for (int i = 0; i < length(); i++) {
+            SNAKE.get(i).paintComponent(g);
+        }
+    }
+    @Override
+    public void run() {
+        while (!gameOver) {
+            //Game code
+        }
+    }
+}

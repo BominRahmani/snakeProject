49595b523d20ce6b5923b6b9c3bedca7b5482310
ky3979 <50038863+ky3979@users.noreply.github.com>
2019-04-29 21:05:19 -0400

    Update Engine.java

--- GroupProject1/Engine.java
+++ GroupProject1/Engine.java
@@ -1,25 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Engine {
 
-    public Engine() {
-        JFrame frame = new JFrame();
-        Board board = new Board();
+    static boolean playing = true;
+    static JFrame frame;
+    static Board board;
 
-        frame.add(board);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Snake Project");
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        board.run();
-        if(board.gameOver){
-            JOptionPane.showMessageDialog(frame, "GameOver");
+    public Engine() {
+        while (playing) {
+            frame = new JFrame();
+            board = new Board();
+            frame.add(board);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("Snake Project");
+            frame.pack();
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+            board.run();
+            if (board.gameOver) {
+                int input = JOptionPane.showConfirmDialog(null,
+                        "Do you want to restart?", "Game Over", JOptionPane.YES_NO_OPTION);
+                if (input == 1) {
+                    playing = false;
+                    System.exit(0);
+                }
+                else
+                    frame.dispose();
+            }
         }
     }
 
     public static void main(String[] args) {
         Engine e = new Engine();
     }
-
 }
